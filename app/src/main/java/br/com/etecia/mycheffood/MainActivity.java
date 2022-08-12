1474858d83cb788eecf9e3bbddc8b6dcbe737224
pedroramos17 <pedro_ramos2003@hotmail.com>
2022-08-11 21:21:50 -0300

    First-commit

---
+++ app/src/main/java/br/com/etecia/mycheffood/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.mycheffood;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
